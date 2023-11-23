35870531be51f6efc2f3491c270dc955457320ea
aserold <oluwoartem@gmail.com>
2023-11-23 11:51:05 +0300

    changed models to perfect "populate_currency_data"

--- converter/models.py
+++ converter/models.py
@@ -5,7 +5,7 @@ class Currency(models.Model):
     code = models.CharField(max_length=3, unique=True)
     name = models.CharField(max_length=50)
     base = models.CharField(max_length=3, default='EUR')
-    exchange_rate = models.DecimalField(max_digits=20, decimal_places=2)
+    exchange_rate = models.DecimalField(max_digits=30, decimal_places=15)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
