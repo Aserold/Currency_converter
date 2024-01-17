66c6397079d5caf12c0191001adf4e31871c8126
Aserold <132985115+Aserold@users.noreply.github.com>
2024-01-17 23:48:10 +0300

    I'm getting pissed

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b+p6viu966s2-v0d_@+d8gysw!y8r#71*2f2-%@=ky7e5ww228'
+SECRET_KEY = 'django-insecure-b+p6viu966s2-v0d_@+d8gysw!y8r#71*2f2-%@=ky7e5ww547'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
